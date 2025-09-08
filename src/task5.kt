4a77093eb5c838125b1f8c27acfdf2a7f469c532
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2025-09-08 20:03:57 +0700

    #1 п5: [x] - реализация через группу вызовов
    Не понял, как делать через sortedBy. Оставил текущий sortedWith

--- src/task5.kt
+++ src/task5.kt
@@ -1,10 +1,12 @@
 fun main(args: Array<String>) {
-    val group = args.groupingBy { it }.eachCount()
+    args.groupingBy { it }
+        .eachCount()
         .toList()
-
-    val sorted = group.sortedWith(
-        compareByDescending<Pair<String, Int>> { it.second }
-            .thenBy { it.first }
-    )
-    sorted.forEach { println("${it.first} ${it.second}") }
+        .sortedWith(
+            compareByDescending<Pair<String, Int>> { it.second }
+                .thenBy { it.first }
+        )
+        .forEach { (word, count) ->
+            println("$word $count")
+        }
 }

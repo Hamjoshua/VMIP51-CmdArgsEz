7ee6c69ad4b326eaec4f4c46d6ead4d91e2b8556
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2025-09-08 19:55:40 +0700

    #1 п3: [x] - реализация в виде одной строчки

--- src/task3.kt
+++ src/task3.kt
@@ -1,9 +1,3 @@
 fun main(args: Array<String>) {
-    val argsSet = args.toSet()
-    val newArgs = argsSet.toTypedArray()
-    newArgs.sort()
-
-    for (i in 0..(newArgs.size - 1)) {
-        println(newArgs[i])
-    }
+    args.distinct().sorted().forEach (::println)
 }
